a33c10acebb906c478210fc1a9a8aa8e36047e5e
Mohammed Ehab <mohamedehabam99@hotmail.com>
2025-06-16 11:20:35 +0300

    .

--- app/(root)/forms/details/[id]/page.tsx
+++ app/(root)/forms/details/[id]/page.tsx
@@ -2,13 +2,13 @@ import { FormDetails } from '@/components/FormDetails';
 import { getFormById } from '@/lib/actions/forms';
 
 const page = async ({ params }: { params: { id: string } }) => {
-  const { id } = params;
-  const form = (await getFormById(id)).form as unknown as Form;
+  const formResponse = await getFormById(params.id);
+  const form = formResponse.form as unknown as Form;
 
   return (
     <FormDetails
       totalResponses={form?.responses?.length || 0}
-      id={id}
+      id={params.id}
       responses={form?.responses || []}
     />
   )
