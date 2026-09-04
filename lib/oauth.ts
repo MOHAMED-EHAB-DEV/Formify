3b60cff5982b749a4d39b436ab2e12ecbc0db068
Mohammed Ehab <mohamed9919698@gmail.com>
2026-09-04 14:00:03 +0300

    commit

--- lib/oauth.ts
+++ lib/oauth.ts
@@ -1,6 +1,6 @@
 import { Google, GitHub } from 'arctic';
 
-const appUrl = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
+const appUrl = (process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000').replace(/\/+$/, '');
 
 export const googleOAuth = new Google(
   process.env.GOOGLE_CLIENT_ID || '',
